2b9b4793cea4d63305e173b5b4ab04451ebb16a8
Azarill <heierms@gmail.com>
2022-08-13 17:40:27 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router forceRefresh={true}>
+      <Router >
       
           <Navbar />
             <Routes>
