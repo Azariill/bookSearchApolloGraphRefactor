614965e6edbc8352e746d821adef17037d90e42f
Azarill <heierms@gmail.com>
2022-08-13 16:56:03 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -33,12 +33,12 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router forceRefresh={true}>
+      <Router >
       
           <Navbar />
             <Switch>
               <Route exact path='/' component={SearchBooks} />
-              <Route exact path='/saved' component={SavedBooks} />
+              <Route path='/saved' component={SavedBooks} />
               <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
       
